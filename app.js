d7b231ab3c0703f2feb20a562268f01896008802
Neous1 <Neous1@users.noreply.github.com>
2017-09-16 12:54:19 -0400

    export a method ask in question.js to app.js
    make ask() available to app.js with require

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
+
 require("./instantHello");
 
 var goodbye = require("./talk/goodbye");
 var talk = require("./talk");
+var question = require("./talk/question");
 
 talk.intro();
+talk.hello("Yvon");
 
+var answer = question.ask("What is the meaning of life?");
+console.log(answer);
 
 goodbye();
