d830a7e76bff016b489d70e5a4bb496a646ca6e1
Neous1 <Neous1@users.noreply.github.com>
2017-09-16 15:57:14 -0400

    Add named callback for code clarity

--- Async/readFileAsync.js
+++ Async/readFileAsync.js
@@ -1,10 +1,11 @@
 var fs = require("fs");
 
+var onFileLoad = function(err, file){
+    
+    console.log("Got the file");
+}
 console.log("Going to get a file");
 
-fs.readFile("readFileSync.js", function(err, file){
-    console.log("Got the file");
-
-});
+fs.readFile("readFileSync.js", onFileLoad)
 
 console.log("App continues...");
