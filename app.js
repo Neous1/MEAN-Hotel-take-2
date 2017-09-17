7e349cf2d7be35bfac1b83b00915aa180d9cb39e
Neous1 <Neous1@users.noreply.github.com>
2017-09-17 08:23:21 -0400

    app.listen returns an object

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ var app = express();
 
 app.set("port", 3000);
 
-app.listen(app.get("port"), function(){
-  console.log("Live on port "+app.get("port"));
+//app.listen returns an object that can be used to extract port number
+var server = app.listen(app.get("port"), function(){
+    var port = server.address().port; // extract port number from port object
+  console.log("Live on port " + port);
 });
-
-console.log("Me first");
