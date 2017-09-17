0380da5ce54c91ae86ec56b723d7ee6acee3a8cb
Neous1 <Neous1@users.noreply.github.com>
2017-09-17 18:25:41 -0400

    require the json file to the controller to be served

--- api/controllers/hotels.controllers.js
+++ api/controllers/hotels.controllers.js
@@ -1,8 +1,9 @@
+// a json file need only be required to be available unlike a text file
+var hotelData = require("../data/hotel-data.json");
+
 module.exports.hotelsGetAll = function(req, res){
-    console.log("GET the json")
+    console.log("GET the hotels")
     res
         .status(200)
-        .json({
-            "jsonDatat": true
-        });
+        .json(hotelData);
 }
