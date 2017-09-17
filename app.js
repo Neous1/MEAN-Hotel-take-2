dcd7aca0f6cb344c00a2226f4572621f9d4e074d
Neous1 <Neous1@users.noreply.github.com>
2017-09-17 10:42:26 -0400

    setting up static page serving static file

--- app.js
+++ app.js
@@ -5,26 +5,24 @@ var path = require("path");
 
 app.set("port", 3000);
 
-app.get("/", function(req, res){
-    console.log("Get the homepage");
-    res
-    .status(200)
-    .sendFile(path.join(__dirname, "public", "index.html"))
-});
-app.get("/json", function(req, res){
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/json", function (req, res) {
     console.log("Get the json");
     res
-    .status(202)
-    .json({"jsonData": true});
+        .status(202)
+        .json({
+            "jsonData": true
+        });
 });
-app.get("/file", function(req, res){
+app.get("/file", function (req, res) {
     console.log("Get the file");
     res
-    .status(202)
-    .sendFile(path.join(__dirname, "app.js"));// responds with content of app.js
+        .status(202)
+        .sendFile(path.join(__dirname, "app.js")); // responds with content of app.js
 });
 
-var server = app.listen(app.get("port"), function(){
+var server = app.listen(app.get("port"), function () {
     var port = server.address().port; // extract port number from port object
-  console.log("Live on port " + port);
+    console.log("Live on port " + port);
 });
