95a5555a86ea32f2695ebe06fd1783b3983ce79d
Neous1 <Neous1@users.noreply.github.com>
2017-09-17 09:08:40 -0400

    add path module and routing

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require("express");
 var app = express();
+var path = require("path");
+
 
 app.set("port", 3000);
 
@@ -14,6 +16,12 @@ app.get("/json", function(req, res){
     res
     .status(202)
     .json({"jsonData": true});
+});
+app.get("/file", function(req, res){
+    console.log("Get the file");
+    res
+    .status(202)
+    .sendFile(path.join(__dirname, "app.js"));// responds with content of app.js
 });
 
 var server = app.listen(app.get("port"), function(){
