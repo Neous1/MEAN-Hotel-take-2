538548d3a41b270063cdf0391f344cd494e4359a
Neous1 <Neous1@users.noreply.github.com>
2017-09-17 08:49:56 -0400

    routing

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ var app = express();
 
 app.set("port", 3000);
 
-//app.listen returns an object that can be used to extract port number
+app.get("/", function(req, res){
+    console.log("Get the homepage");
+    res.send("Response sent to sever")
+});
+
 var server = app.listen(app.get("port"), function(){
     var port = server.address().port; // extract port number from port object
   console.log("Live on port " + port);
