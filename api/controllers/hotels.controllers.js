69b25f74bf60b08d771a3a832906478bb79c0175
Neous1 <Neous1@users.noreply.github.com>
2017-09-18 12:32:26 -0400

    connect controller to db and the collection targeted use the findOne method to display 1 record
    nb, as coded it will display 1 record of the data queried not necessarily the right data

--- api/controllers/hotels.controllers.js
+++ api/controllers/hotels.controllers.js
@@ -30,26 +30,20 @@ module.exports.hotelsGetAll = function (req, res) {
         });
 
 
-    // console.log("db", db);
-
-    // console.log("GET the hotels")
-    // console.log(req.query);
-
-
-    //slice is used to determine the starting and ending point of amount of data being displayed/served
-    // var returnData = hotelData.slice(offset, offset + count)
-    // res
-    //     .status(200)
-    //     .json(returnData);
 };
 
 module.exports.hotelsGetOne = function (req, res) {
+    var db = dbconn.get();
+    var collection = db.collection("hotels");
+
     var hotelId = req.params.hotelId; //extract the url parameter and save it in a variable. which can be used to get a specific piece of data
-    var thisHotel = hotelData[hotelId]; //hotelId is being used as a location index on the hotel-data array 
     console.log("GET hotelId", hotelId);
-    res
-        .status(200)
-        .json(thisHotel);
+    collection
+        .findOne({}, function (err, doc) {
+            res
+                .status(200)
+                .json(doc);
+        })
 };
 
 module.exports.hotelsAddOne = function (req, res) {
