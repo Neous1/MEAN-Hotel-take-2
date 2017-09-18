76c416f001dd5759472fd33ea40b6c70b03747c8
Neous1 <Neous1@users.noreply.github.com>
2017-09-18 11:21:40 -0400

    require mongoclient to work with connection to database

--- api/data/dbconnections.js
+++ api/data/dbconnections.js
@@ -1,8 +1,18 @@
+var MongoClient = require("mongodb").MongoClient;
+
+//protocol/server:port/dbname
+var dburl = "mongodb://localhost:27017/meanhotel";
+
 var _connection = null;
 
 var open = function(){
-
+// set _connection
 };
+
+var get = function(){
+    return _connection;
+};
+
 module.exports= {
     open:open,
     get:get
