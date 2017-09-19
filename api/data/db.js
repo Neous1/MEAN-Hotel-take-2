15fc5ec63207dddb761c3208018fbe4089820da3
Neous1 <Neous1@users.noreply.github.com>
2017-09-19 01:21:48 -0400

    close mongoose connections

--- api/data/db.js
+++ api/data/db.js
@@ -12,6 +12,27 @@ mongoose.connection.on("disconnected", function(){
     console.log("Mongoose disconnected ");
 });
 
-mongoose.connection.on("error", function(){
+mongoose.connection.on("error", function(err){
     console.log("Mongoose connection error: " + error);
 });
+
+process.on('SIGINT', function () {
+    mongoose.connection.close(function() {
+      console.log("Mongoose disconnected through app termination (SIGINT)");
+      process.exit(0);
+    });
+  });
+  
+  process.on('SIGTERM', function () {
+    mongoose.connection.close(function() {
+      console.log("Mongoose disconnected through app termination (SIGTERM)");
+      process.exit(0);
+    });
+  });
+  
+  process.on('SIGUSR2', function() {
+    mongoose.connection.close(function() {
+      console.log('Mongoose disconnected through app termination (SIGUSR2)');
+      process.kill(process.pid, 'SIGUSR2');
+    });
+  });
