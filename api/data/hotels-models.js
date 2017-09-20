477460f83778da5b68ef90a863669e5096181118
Neous1 <Neous1@users.noreply.github.com>
2017-09-20 14:58:31 -0400

    use the long form of schema type to be more precise

--- api/data/hotels-models.js
+++ api/data/hotels-models.js
@@ -2,10 +2,18 @@
 var mongoose = require("mongoose");
 
 var hotelSchema = new mongoose.Schema({
-    name: String, //name is a path and string is a schema type
-    stars: Number,
+    name: {
+        type: String,
+        required: true
+    }, //name is a path and string is a schema type
+    stars: {
+        type: Number,
+        min: 0,
+        max: 5,
+        default: 0
+    },
     services: [String],
     description: String,
     photos: [String],
     currency: String
-})
+});
