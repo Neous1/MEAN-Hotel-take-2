ee3e51e87f32bd63e5a618140f1e792eefd32912
Neous1 <Neous1@users.noreply.github.com>
2017-09-20 14:50:56 -0400

    create a new schema for this model

---
+++ api/data/hotels-models.js
@@ -0,0 +1,6 @@
+//this model is what gets exported from this file rather that schema
+var mongoose = require("mongoose");
+
+var hotelSchema = new mongoose.Schema({
+    name: String //name is a path and string is a schema type
+})
