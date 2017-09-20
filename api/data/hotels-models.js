7311238ddc6f3f965a565d41b21ded940aa287c8
Neous1 <Neous1@users.noreply.github.com>
2017-09-20 15:25:20 -0400

    add a reviewSchema
    reference it in the main schema

--- api/data/hotels-models.js
+++ api/data/hotels-models.js
@@ -1,6 +1,23 @@
 //this model is what gets exported from this file rather that schema
+
 var mongoose = require("mongoose");
 
+var reviewSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },    
+    rating: {
+        type: Number,
+        min: 0,
+        max: 5,
+        default: 0
+    },
+    createdOn: {
+        type: Date,
+        "default": Date.now
+    }
+});
 var hotelSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -15,7 +32,8 @@ var hotelSchema = new mongoose.Schema({
     services: [String],
     description: String,
     photos: [String],
-    currency: String
+    currency: String,
+    reviews: [reviewSchema]
 });
 
 mongoose.model("Hotel", hotelSchema, "hotels");
